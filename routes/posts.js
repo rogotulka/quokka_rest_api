f587f1d336a609ab814587ba7ec3ee8555f37a1c
rogotulka <rogotulka@gmail.com>
2021-11-09 22:13:54 +0300

    add v0.1

---
+++ routes/posts.js
@@ -0,0 +1,25 @@
+module.exports = {
+    getPosts(req, res) {
+        res.status(200).send(req.store.posts)
+    },
+    addPost(req, res) {
+        req.store.posts.push(req.body)
+        res.status(201).send({"id": req.store.posts.length})
+    },
+    updatePost(req, res) {
+        const id = req.params.postId
+        console.log(id)
+        req.store.posts[id] = req.body
+        console.log(req.body)
+        res.status(200).send(req.store.posts[id])
+    },
+    getPost(req, res) {
+        const id = req.params.postId
+        // req.store.posts[id] = req.body
+        res.status(200).send(req.store.posts[id])
+    },
+    removePost(req, res) {
+        req.store.posts.splice(req.params.postId, 1)
+        res.status(204).send()
+    }
+}
